116c7b502ef6b6939a3408746b26310f3029eaa9
takatori <takatorisatoshi@team-lab.com>
2017-01-22 13:39:23 +0900

    Add main template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate x86emulator_rs;
+
+    
+enum registers_name {
+    EAX,
+    ECX,
+    EDX,
+    EBX,
+    ESP,
+    EBP,
+    ESI,
+    EDI
+}
+
+const MEMORY_SIZE: i32 = 1024 * 1024;
+
+
+fn read_binary(emu: &Emulator, filename: &str) {
+    
+}
+
+fn dump_registers(emu: &Emulator) {
+    
+}
+
+
+fn create_emu(size: usize, eip: u32, esp: u32) -> Emulator {
+    
+}
+
+
+fn destroy_emu(emu: &Emulator) {
+    
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
