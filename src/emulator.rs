ed245e4d0887492e9d56a5f9f157bcd00a0bda38
takatori <takatorisatoshi@team-lab.com>
2017-01-24 21:29:24 +0900

    Define memory as mutable array

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,5 +1,6 @@
 
-const REGISTERS_COUNT:usize = 8;
+const REGISTERS_COUNT: usize = 8;
+const MEMORY_SIZE: usize = 1024 * 1024;
 
 pub enum Register {
     EAX,
@@ -19,13 +20,13 @@ impl Register {
     pub const DL: Register = Register::EDX;
     pub const BL: Register = Register::EBX;
 
-    pub const AH: Register = Register::EAX as i32 + 4; //本当は AL + 4と書きたいが ``not found in scope``になる
-    pub const CH: Register = CL + 4;
-    pub const DH: Register = DL + 4;
-    pub const BH: Register = BL + 4;
+    // pub const AH: Register = Register::EAX as i32 + 4; //本当は AL + 4と書きたいが ``not found in scope``になる
+    // pub const CH: Register = CL + 4;
+    // pub const DH: Register = DL + 4;
+    // pub const BH: Register = BL + 4;
 }
 
-struct Emulator<'a> {
+struct Emulator {
 
     // 汎用レジスタ
     registers: [u32; REGISTERS_COUNT],     // 本当は[u32; Register::REGISTERS_COUNT],のようにしたいが、unresolved path in constant expressionになる
@@ -34,7 +35,7 @@ struct Emulator<'a> {
     eflags: u32,
 
     // メモリ
-    memory: &'a u8,
+    memory: [u8; MEMORY_SIZE], // 生ポインタを使用する -> 配列で宣言する
 
     // プログラムカウンタ
     eip: u32,
@@ -42,31 +43,66 @@ struct Emulator<'a> {
 }
 
 
-impl <'a>Emulator<'a> {
+impl Emulator {
 
-    fn push32(&self, value: u32) {
+    pub fn new() -> Emulator {
 
-        let address:u32 =  self.get_register32(Register::ESP as usize) - 4;
+        Emulator {
+            registers: [0; REGISTERS_COUNT],
+            eflags: 0,
+            memory: [0; MEMORY_SIZE],
+            eip: 0
+        }
+    }
+    
+    pub fn push32(&mut self, value: u32) {
         
+        let address: u32 = self.get_register32(Register::ESP as u8) - 4;
+        self.set_register32(Register::ESP as u8, address);
+        self.set_memory32(address, value);
         
     }
 
-    fn pop32(&self) -> u32 {
-        
+    pub fn pop32(&mut self) -> u32 {
+
+        let address: u32 = self.get_register32(Register::ESP as u8);
+        let ret: u32     = self.get_memory32(address);
+        self.set_register32(Register::ESP as u8, address + 4);
+        ret
     }
 
-    fn get_code8(&self, index: i32) -> i32 {
-        
+    // memory配列の指定した番地から8ビットの値を取得する関数
+    pub fn get_code8(&self, index: i32) -> u8 {
+        self.memory[(self.eip + index) as usize]
     }
 
 
-    fn get_register32(self, index: usize) -> u32 {
-        return self.registers[index];
+    pub fn get_register32(&self, index: u8) -> u32 {
+        return self.registers[index as usize];
     }
 
 
-    fn set_register32(&self, index: usize, value: u32) {
-        self.registers[index] = value;
+    pub fn set_register32(&mut self, index: u8, value: u32) {
+        self.registers[index as usize] = value;
+    }
+
+    
+    pub fn get_memory8(&self, address: u32) -> u8 {
+        self.memory[address as usize]
+    }
+
+    pub fn get_memory32(&self, address: u32) -> u32 {
+        (0..4).fold(0, |acc, i| acc | (self.get_memory8(address + i) as u32) << (8 * i))
+    }
+
+     fn set_memory8(&mut self, address: u32, value: u32) {
+        self.memory[address as usize] = (value & 0xFF) as u8 // u32からu8へのキャストは頭を切り詰められる
+    }
+
+    pub fn set_memory32(&mut self, address: u32, value: u32) {
+        for i in 0..4 {
+            self.set_memory8(address + i, value >> (i * 8))
+        }
     }
 
     
